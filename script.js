b781d7924d9b3cb81cb07cfdc34e34ae1b768143
Irbis007 <suskodomk@gmail.com>
2023-05-15 21:30:08 +0300

    change blurtooth connection

--- script.js
+++ script.js
@@ -5,21 +5,11 @@ const button = document.querySelector('button')
 
 
 button.addEventListener('click', (event)=>{
-    let options = {
-        filters: [
-          { services: ["heart_rate"] },
-          { services: [0x1802, 0x1803] },
-          { services: ["c48e6067-5295-48d3-8d5c-0395f61792b1"] },
-          { name: "ExampleName" },
-          { namePrefix: "Prefix" },
-        ],
-        optionalServices: ["battery_service"],
-      };
-      
+
       navigator.bluetooth
-        .requestDevice(options)
+        .requestDevice()
         .then((device) => {
-          console.log(`Name: ${device.name}`);
+          console.log(device);
           // Do something with the device.
         })
         .catch((error) => console.error(`Something went wrong. ${error}`));
